2ca63c3579e8679db39987aec07dcf7aa16c5330
fadelananda <fadelananda123@gmail.com>
2021-03-01 02:49:11 +0700

    add function to add "modularity"

--- src/13519146.py
+++ src/13519146.py
@@ -2,39 +2,9 @@
 # NIM   : 13519146
 # Kelas : K-03
 
-# Asumsi test case sudah DAG
-filename = input("Masukkan nama file: ")
-try:
-    listMatkul = open("../test/"+filename, "r").readlines() #membuka file dan memasukkannya ke array listMatkul
-
-    vertex = [] #berisi semua simpul unik yang ada pada soal
-    connectedTo = [] #berisi simpul yang bertetangga dengan simpul di list vertex
-    numberOfConnectedVertex = [] #berisi derajat masuk simpul2 yang ada
-
-    for i in range(len(listMatkul)): #mencari semua vertex dari text file dan dimasukkan ke dalam vertex
-        stored = []
-        listMatkul[i] = listMatkul[i].strip().replace('.', '').replace(' ', '').rsplit(",")
-        for j in range(len(listMatkul[i])):
-            if(j!=0):
-                stored.append(listMatkul[i][j])
-            else:
-                vertex.append(listMatkul[i][j])
-        numberOfConnectedVertex.append(len(stored))
-        connectedTo.append(stored)
-    print("--------------------------------------------------------------------------------------------\n")
-    print("Simpul-simpul yang ada pada graf: ", vertex)
-    for i in range(len(vertex)):
-        print("Simpul lain yang masuk ke dalam simpul ", vertex[i], " :", connectedTo[i])
-    print("")
-    print("--------------------------------------------------------------------------------------------\n")
-    print("Jumlah derajat masuk masing2 simpul:", numberOfConnectedVertex)
-    print()
-    print("--------------------------------------------------------------------------------------------")
-
-    print("Matkul yang dapat diambil :")
-    semester=0 #semester
-
-    #Topological Sort
+# Asumsi test case sudah DAG (tidak ada syclical graph)
+def topologicalSort(vertex, connectedTo, numberOfConnectedVertex):
+    semester = 0
     while(len(vertex)!=0 and semester<8):
         solution = [] #berisi solusi dari permasalahan
         for i in range(len(numberOfConnectedVertex)): #cari yang derajat 0 masukkan ke list solusi sementara
@@ -71,6 +41,45 @@ try:
 
         print("Semester", semester+1 ,"mahasiswa dapat mengambil matkul: ", solution) #solusi
         semester += 1
+
+def createGraph(filename):
+    listMatkul = open("../test/"+filename, "r").readlines() #membuka file dan memasukkannya ke array listMatkul
+
+    vertex = [] #berisi semua simpul unik yang ada pada soal
+    connectedTo = [] #berisi simpul yang bertetangga dengan simpul di list vertex
+    numberOfConnectedVertex = [] #berisi derajat masuk simpul2 yang ada
+
+    for i in range(len(listMatkul)): #mencari semua vertex dari text file dan dimasukkan ke dalam vertex
+        stored = []
+        listMatkul[i] = listMatkul[i].strip().replace('.', '').replace(' ', '').rsplit(",")
+        for j in range(len(listMatkul[i])):
+            if(j!=0):
+                stored.append(listMatkul[i][j])
+            else:
+                vertex.append(listMatkul[i][j])
+        numberOfConnectedVertex.append(len(stored))
+        connectedTo.append(stored)
+    print("--------------------------------------------------------------------------------------------\n")
+    print("Simpul-simpul yang ada pada graf: ", vertex)
+    for i in range(len(vertex)):
+        print("Simpul lain yang masuk ke dalam simpul ", vertex[i], " :", connectedTo[i])
+    print("")
+    print("--------------------------------------------------------------------------------------------\n")
+    print("Jumlah derajat masuk masing2 simpul:", numberOfConnectedVertex)
+    print()
+    print("--------------------------------------------------------------------------------------------")
+
+    print("Matkul yang dapat diambil :")
+    return vertex, connectedTo, numberOfConnectedVertex
+
+#Main Program
+filename = input("Masukkan nama file: ")
+try:
+    vertex, connectedTo, numberOfConnectedVertex = createGraph(filename)
+    semester=0 #semester
+
+    #Topological Sort
+    topologicalSort(vertex, connectedTo, numberOfConnectedVertex)
 
 except:
     print("File tidak ditemukan")
